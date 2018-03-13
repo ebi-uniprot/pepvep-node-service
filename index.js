53102e44a9983083858879ca3a76f35308e0d6f5
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-03-13 11:47:36 +0000

    added Axios plus a sample request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const axios = require('axios');
 
 const app = express();
 const port = 3687;
@@ -12,5 +13,13 @@ app.use(bodyParser.json());
 
 // Is Alive?
 app.get('/is-alive', (req, res) => res.send('yes!'));
+
+// Example
+app.get('/example', (req, res) => {
+  const url = 'https://www.ebi.ac.uk/proteins/api/coordinates/9606/1:121593709-121594999?offset=0&size=100&format=json';
+  
+  axios.get(url)
+    .then(response => res.send(response.data));
+});
 
 app.listen(port, () => console.log(`server listening on http://localhost:${port}/`));
