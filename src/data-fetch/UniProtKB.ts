b06be8adc4b66f2e38fbaa6314061a1f95181688
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-11-19 14:33:29 +0000

    fixed TSLint issues in

--- src/data-fetch/UniProtKB.ts
+++ src/data-fetch/UniProtKB.ts
@@ -1,24 +1,26 @@
-import axios from "axios";
-import Helpers from "./Helpers";
+import axios from 'axios';
+import Helpers from './Helpers';
 
-const taxID = "9606";
+const taxID = '9606';
 
 export default class UniProtKB {
   public static async proteinsDetailByAccession(accessions: string[]) {
-    let queryString: string = Helpers.stringOrArrayToCommaSeparated(accessions);
-    const url: string = `https://www.ebi.ac.uk/proteins/api/proteins?format=json&accession=${queryString}`;
+    const queryString: string = Helpers.stringOrArrayToCommaSeparated(accessions);
+    const url: string =
+      `https://www.ebi.ac.uk/proteins/api/proteins?format=json&accession=${queryString}`;
+
     return await axios.get(url);
   }
 
   public static async getProteinFeatures(accessions: string[]) {
-    const accessionsString: string = accessions.join(",");
+    const accessionsString: string = accessions.join(',');
     // TODO this call is limited to 100 only, we need to do it in batches
     const url: string = `https://www.ebi.ac.uk/proteins/api/features?accession=${accessionsString}`;
     return await axios.get(url);
   }
 
   public static async getProteinVariants(accessions: string[]) {
-    const accessionsString: string = accessions.join(",");
+    const accessionsString: string = accessions.join(',');
     // TODO this call is limited to 100 only, we need to do it in batches
     const url: string = `https://www.ebi.ac.uk/proteins/api/variation/${accessionsString}`;
     return await axios.get(url);
