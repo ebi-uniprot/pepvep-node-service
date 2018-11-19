ac76d90f89cc44e45b62cf07b272d414ccd15e28
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-11-19 16:14:55 +0000

    fixed TSLint issues for

--- src/data-structure/Protein.ts
+++ src/data-structure/Protein.ts
@@ -1,26 +1,26 @@
-import Varition from "./Variation";
+import Variation from './Variation';
 
 export default class Protein {
   readonly accession: string;
   private _ensp: string;
   private _enst: string;
-  private _variations: Varition[] = [];
+  private _variations: Variation[] = [];
 
   constructor(accession: string) {
     this.accession = accession;
   }
 
-  public get ensp() : string { return this._ensp; };
-  public set ensp(ensp: string) { this._ensp = ensp; };
+  public get ensp() : string { return this._ensp; }
+  public set ensp(ensp: string) { this._ensp = ensp; }
 
-  public get enst() : string { return this._enst; };
-  public set enst(enst: string) { this._enst = enst; };
+  public get enst() : string { return this._enst; }
+  public set enst(enst: string) { this._enst = enst; }
 
-  public addVariation(variation: Varition) {
+  public addVariation(variation: Variation) {
     this._variations.push(variation);
   }
 
-  public getVariations(): Varition[] {
+  public getVariations(): Variation[] {
     return this._variations;
   }
 }
