0627f2d0cace3932833108dc44259ff697d855d0
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-11-19 16:24:48 +0000

    fixed TSLint issues for

--- src/data-process/Significance.ts
+++ src/data-process/Significance.ts
@@ -1,34 +1,34 @@
-import UniProtKB from "../data-fetch/UniProtKB";
-import Protein from "../data-structure/Protein";
-import Feature from "../data-structure/significance/Feature";
-import Evidence from "../data-structure/significance/Evidence";
+import UniProtKB from '../data-fetch/UniProtKB';
+import Protein from '../data-structure/Protein';
+import Feature from '../data-structure/significance/Feature';
+import Evidence from '../data-structure/significance/Evidence';
 
 export default class Significance {
-  public static async addPositionalSignificance(proteins: Array<Protein>) {
+  public static async addPositionalSignificance(proteins: Protein[]) {
     const accessionList = proteins.map(p => p.accession);
     const significance = await UniProtKB.getProteinFeatures(accessionList).then(
       results =>
-        results.data.forEach(proteinResult => {
+        results.data.forEach((proteinResult) => {
           const protein: Protein = proteins.find(
-            p => p.accession === proteinResult.accession
+            p => p.accession === proteinResult.accession,
           );
           const variations = protein.getVariations();
-          variations.forEach(variation => {
+          variations.forEach((variation) => {
             variation.addOverlappingFeatures(proteinResult.features);
           });
-        })
+        }),
     );
   }
 
   public static parseHGVS(hgvsString: string) {
     // NC_000021.9:g.25905030G>C
-    var regex = /([\w.-]+)(:)([cgmnpr])\.(\d+)(.)>(.)/g;
+    const regex = /([\w.-]+)(:)([cgmnpr])\.(\d+)(.)>(.)/g;
     const matches = regex.exec(hgvsString);
     return {
       chromosome: matches[1],
-      position: parseInt(matches[4]),
+      position: parseInt(matches[4], 10),
       wt: matches[5],
-      allele: matches[6]
+      allele: matches[6],
     };
   }
 
