09b7dd7beab34169a9f662b5fc6c2abcd43af986
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-11-19 16:06:00 +0000

    fixed TSLint issues for

--- src/data-structure/significance/ColocatedVariant.ts
+++ src/data-structure/significance/ColocatedVariant.ts
@@ -11,7 +11,7 @@ export default class ColocatedVariant {
     this.id = id;
   }
 
-  get pubMedIDs() : string[] { return this._pubMedIDs; };
+  get pubMedIDs() : string[] { return this._pubMedIDs; }
 
   public addPubMedID(id: string) : void {
     this._pubMedIDs.push(id);
