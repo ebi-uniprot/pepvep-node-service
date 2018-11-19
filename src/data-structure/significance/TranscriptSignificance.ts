18d7540c72311595e581d60eb1222f04a7c30dbd
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-11-19 15:57:00 +0000

    fixed TSLint issues for

--- src/data-structure/significance/TranscriptSignificance.ts
+++ src/data-structure/significance/TranscriptSignificance.ts
@@ -16,28 +16,28 @@ export default class TranscriptSignificance {
   private _siftScore: number;
   private _consequenceTerms: string[] = [];
 
-  get biotype() : string { return this._biotype; };
-  set biotype(type: string) { this._biotype = type; };
+  get biotype() : string { return this._biotype; }
+  set biotype(type: string) { this._biotype = type; }
 
-  get impact() : string { return this._biotype; };
-  set impact(type: string) { this._impact = type; };
+  get impact() : string { return this._impact; }
+  set impact(type: string) { this._impact = type; }
 
-  get codons() : string { return this._codons; };
-  set codons(codons: string) { this._codons = codons; };
+  get codons() : string { return this._codons; }
+  set codons(codons: string) { this._codons = codons; }
 
-  get polyphenPrediction() : string { return this._polyphenPrediction; };
-  set polyphenPrediction(prediction: string) { this._polyphenPrediction = prediction; };
+  get polyphenPrediction() : string { return this._polyphenPrediction; }
+  set polyphenPrediction(prediction: string) { this._polyphenPrediction = prediction; }
 
-  get polyphenScore() : number { return this._polyphenScore; };
-  set polyphenScore(score: number) { this._polyphenScore = score; };
+  get polyphenScore() : number { return this._polyphenScore; }
+  set polyphenScore(score: number) { this._polyphenScore = score; }
 
-  get siftPrediction() : string { return this._siftPrediction; };
-  set siftPrediction(prediction: string) { this._siftPrediction = prediction; };
+  get siftPrediction() : string { return this._siftPrediction; }
+  set siftPrediction(prediction: string) { this._siftPrediction = prediction; }
 
-  get siftScore() : number { return this._siftScore; };
-  set siftScore(score: number) { this._siftScore = score; };
+  get siftScore() : number { return this._siftScore; }
+  set siftScore(score: number) { this._siftScore = score; }
 
-  get consequenceTerms() : string[] { return this._consequenceTerms; };
+  get consequenceTerms() : string[] { return this._consequenceTerms; }
 
   public addConsequenceTerm(term: string) {
     this._consequenceTerms.push(term);
