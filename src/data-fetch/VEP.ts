2d67f80114249a77147763b3829d062d50772342
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-11-19 16:31:40 +0000

    fixed TSLint issues for

--- src/data-fetch/VEP.ts
+++ src/data-fetch/VEP.ts
@@ -3,21 +3,27 @@ import axios from 'axios';
 
 export default class VEP {
   public static async variantConsequencesAllInputs(organism: string, input: string) {
-    const url: string = `https://rest.ensembl.org/vep/${organism}/region?content-type=application/json`;
+    const url: string =
+      `https://rest.ensembl.org/vep/${organism}/region?content-type=application/json`;
+
     return await axios.post(url, {
       variants: [input],
       protein: true,
-      uniprot: true
+      uniprot: true,
     });
   }
 
-  public static async variantConsequences(organism: string, region: string, allele: string ) {
+  public static async variantConsequences(organism: string, region: string, allele: string) {
+    // tslint:disable-next-line:max-line-length
     const url: string = `https://rest.ensembl.org/vep/${organism}/region/${region}/${allele}?content-type=application/json`;
+
     return await axios.get(url);
   }
 
   public static async variantConsequencesBatch(organism: string, variants: string[]) {
-    const url: string = `https://rest.ensembl.org/vep/${organism}/region?content-type=application/json`;
-    return await axios.post(url, {variants});
+    const url: string =
+      `https://rest.ensembl.org/vep/${organism}/region?content-type=application/json`;
+
+    return await axios.post(url, { variants });
   }
 }
