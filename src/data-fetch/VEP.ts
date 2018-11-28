c63788968a2741c7429c390f00b9aa687eb535c1
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2018-11-28 13:23:43 +0000

    attempting to fix the problem with https-over-http error on the remote server

--- src/data-fetch/VEP.ts
+++ src/data-fetch/VEP.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default class VEP {
   public static async variantConsequencesAllInputs(organism: string, input: string) {
     const url: string =
-      `https://rest.ensembl.org/vep/${organism}/region?content-type=application/json`;
+      `https://rest.ensembl.org:443/vep/${organism}/region?content-type=application/json`;
 
     return await axios.post(url, {
       variants: [input],
@@ -15,14 +15,14 @@ export default class VEP {
 
   public static async variantConsequences(organism: string, region: string, allele: string) {
     // tslint:disable-next-line:max-line-length
-    const url: string = `https://rest.ensembl.org/vep/${organism}/region/${region}/${allele}?content-type=application/json`;
+    const url: string = `https://rest.ensembl.org:443/vep/${organism}/region/${region}/${allele}?content-type=application/json`;
 
     return await axios.get(url);
   }
 
   public static async variantConsequencesBatch(organism: string, variants: string[]) {
     const url: string =
-      `https://rest.ensembl.org/vep/${organism}/region?content-type=application/json`;
+      `https://rest.ensembl.org:443/vep/${organism}/region?content-type=application/json`;
 
     return await axios.post(url, { variants });
   }
