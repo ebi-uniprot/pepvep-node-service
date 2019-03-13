303292b0705f908fb589ed5d5bbf95ce662979ec
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2019-03-13 16:15:51 +0000

    another attempt to fix the forward-slash problem

--- src/index.ts
+++ src/index.ts
@@ -20,16 +20,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Enforce a forward-slash at the end of URL
-app.use((req, res, next) => {
-  if (req.path.substr(-1) !== '/') {
-    var query = req.url.slice(req.path.length);
-    res.redirect(301, req.path + '/' + query);
-  } else {
-    next();
-  }
-});
-
 app.use(express.static('public'));
 app.use('/uniprot/pepvep', express.static(path.join(__dirname, '/../../www')));  // to server front-end files from server
 app.use(express.static(path.join(__dirname, '/../../www')));  // to server front-end files from server
@@ -37,6 +27,20 @@ app.use(bodyParser.urlencoded({
   extended: false,
 }));
 app.use(bodyParser.json());
+
+
+// Enforce a forward-slash at the end of URL
+app.use((req, res, next) => {
+  console.log("path:", req.path);
+  if (req.path.substr(-1) !== '/') {
+    const query = req.url.slice(req.path.length);
+    const newUri = req.path + '/' + query;
+    res.redirect(301, newUri);
+  } else {
+    next();
+  }
+});
+
 
 // Is Alive?
 app.get('/is-alive', (req, res) => res.send('Node service is alive.'));
