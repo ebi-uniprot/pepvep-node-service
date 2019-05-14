93dd5fd4ec7b9578cfd83c45919e183899b0e6d4
Mahdi Mahmoudy <mmahmoudy@ebi.ac.uk>
2019-05-14 15:56:42 +0100

    collecting protein feature id

--- src/data-structure/Variation.ts
+++ src/data-structure/Variation.ts
@@ -361,6 +361,7 @@ export default class Variation {
         rawFeature.begin,
         rawFeature.end,
         evidences,
+        rawFeature.ftId,
       );
 
       this.getPositionalSignificance()
